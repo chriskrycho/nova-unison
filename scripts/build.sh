3dc71379dc318e5ae000cb93c8d179afc3bb1f7d
Chris Krycho <hello@chriskrycho.com>
2023-08-28 07:11:26 -0600

    Include ad hoc code signing in build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,9 @@
 #!/bin/zsh
+
 $(pwd)/scripts/compile_parser.sh $(pwd) /Applications/Nova.app
+
 mv libtree-sitter-unison.dylib ./Syntaxes
-cp node_modules/tree-sitter-unison/queries/* Queries
+cp node_modules/tree-sitter-unison/queries/* ./Queries
+
+# `--sign -` performs "ad hoc code signing".
+codesign --sign - ./Syntaxes/libtree-sitter-unison.dylib
