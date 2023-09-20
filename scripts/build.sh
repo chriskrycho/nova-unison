028ffb0b3c0af037617a58549c18f96d3b8de1a8
Chris Krycho <hello@chriskrycho.com>
2023-09-20 08:26:47 -0600

    Do not copy Tree Sitter queries from upstream
    Once we have a flow where the Tree Sitter package *has* these, that
    will be fine, but for now it does not make sense.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,6 @@
 $(pwd)/scripts/compile_parser.sh $(pwd) /Applications/Nova.app
 
 mv libtree-sitter-unison.dylib ./Syntaxes
-cp node_modules/tree-sitter-unison/queries/* ./Queries
 
 # `--sign -` performs "ad hoc code signing".
 codesign --sign - ./Syntaxes/libtree-sitter-unison.dylib
